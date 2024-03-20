4c39a0f6bd8cecdf6a6ad38019542f65ae12b179
ajay <ajay@freshbus.com>
2024-03-21 01:22:56 +0530

    step 1

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,39 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
+o.number = true
+
+-- tabs & indentation
+o.tabstop = 2 -- 2 spaces for tabs (prettier default)
+o.shiftwidth = 2 -- 2 spaces for indent width
+o.expandtab = true -- expand tab to spaces
+o.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+o.wrap = false -- disable line wrapping
+
+-- search settings
+o.ignorecase = true -- ignore case when searching
+o.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+o.cursorline = true -- highlight the current cursor line
+
+-- appearance
+
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+o.termguicolors = true
+o.background = "dark" -- colorschemes that can be light or dark will be made dark
+o.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+o.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- split windows
+o.splitright = true -- split vertical window to the right
+o.splitbelow = true -- split horizontal window to the bottom
+
